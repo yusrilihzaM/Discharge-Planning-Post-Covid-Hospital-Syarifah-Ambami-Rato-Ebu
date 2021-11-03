92ee22376d52b539586f380d3569b23b9aeac0b8
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2021-11-03 13:53:37 +0700

    landing page (index.php)

--- application/views/templates/landing_page/navbar.php
+++ application/views/templates/landing_page/navbar.php
@@ -1,23 +1,19 @@
+<!-- ======= Header ======= -->
+    <header id="header" class="fixed-top">
+        <div class="container d-flex align-items-center">
 
+            <!-- <h6 class="logo mr-auto"><a href="index.html">Discharge Planning</a></h6> -->
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <a href="index.html" class="logo mr-auto"><img src="assets/img/rslogo.png" alt="" class="img-fluid"><small class="ml-3">Discharge Planning</small></a>
 
-  <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top">
-    <div class="container d-flex align-items-center">
-
-      <!-- <h6 class="logo mr-auto"><a href="index.html">Discharge Planning</a></h6> -->
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <a href="index.html" class="logo mr-auto"><img src="assets/img/rslogo.png" alt="" class="img-fluid">Discharge Planning</a>
-
-      <nav class="nav-menu d-none d-lg-block">
-        <ul>
-          <li class="active"><a href="index.html">Beranda</a></li>
-          <li><a href="#about">Tentang</a></li>
-          <li><a href="#services">Layanan</a></li>
-          <li><a href="#contact">Kontak</a></li>
-        </ul>
-      </nav><!-- .nav-menu -->
-
-      <a href="#about" class="get-started-btn scrollto">Masuk</a>
-
-    </div>
-  </header><!-- End Header -->
+            <nav class="nav-menu d-none d-lg-block">
+                <ul>
+                <li class="active"><a href="index.html">Beranda</a></li>
+                <li><a href="#about">Tentang</a></li>
+                <li><a href="#services">Layanan</a></li>
+                <li><a href="#contact">Kontak</a></li>
+                </ul>
+            </nav><!-- .nav-menu -->
+            <a href="#about" class="btn btn-outline-success ml-4">Masuk</a>
+        </div>
+    </header><!-- End Header -->
