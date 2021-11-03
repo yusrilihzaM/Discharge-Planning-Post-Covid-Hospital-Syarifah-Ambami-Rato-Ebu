eb8dcb4c86c2d06e958df09de124253ca4535d3f
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2021-11-03 23:17:43 +0700

    Deteksimandiri1

--- application/views/anonim/deteksi_mandiri/index.php
+++ application/views/anonim/deteksi_mandiri/index.php
@@ -1,10 +1,10 @@
-<div class="container">	
-    <div class="d-flex justify-content-center">
-        <h3 class="mb-3 mt-5">Deteksi Mandiri COVID-19</h3>
-    </div>
-    <div class="d-flex justify-content-center">
-        <img src="assets/img/rslogo.png" alt="" class="align-items-sm-center">
-    </div>
+    <div class="container">	
+        <div class="d-flex justify-content-center">
+            <h3 class="mb-3 mt-5">Deteksi Mandiri COVID-19</h3>
+        </div>
+        <div class="d-flex justify-content-center">
+            <img src="assets/img/rslogo.png" alt="" class="align-items-sm-center">
+        </div>
     <div>
         <h6 class="d-flex justify-content-center mb-2 mt-2 ">Deteksi Mandiri Cepat COVID-19 adalah salah satu cara untuk membantu mempercepat tindakan</h6> 
         <h6 class="d-flex justify-content-center mb-4">apakah Anda sehat atau memiliki gejala yang memerlukan pemeriksaan dan pengujian lebih lanjut mengenai COVID-19</h6>
@@ -14,19 +14,39 @@
 
     </div>
         <form>
-            <div>
-                <button type="button" class="btn btn-primary btn-lg btn-block"><h6>Gejala yang dirasakan</h6></button>
+            <div class="row">
+                <div class="col">
+                        <div class="card bg-success text-white">
+                            <div class="card-body">
+                                Gejala yang dirasakan
+                            </div> 
+                        </div>
+                </div>
             </div>
-            
-            <div class="form-group">
-                <label>Tanda Gejala Hipertensi :</label>
-                <input type="text" class="form-control" placeholder="Pusing berputar">   
-	        </div>
+            <div class="row">
+                <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                Gejala Hipertensi :
+                            </div> 
+                        </div>
+                </div>
+            </div>
+            <div class="row-lg">
+                <div class="card">
+                    <div class="col card-body">
+                        Pusing berputar
+                    </div>
+                    
+                    <div class="form-check">
+                        <label class="radio-inline mr-3"><input type="radio" name="optradio" checked>Ya</label>
+                        <label class="radio-inline"><input type="radio" name="optradio">Tidak</label>
+                    </div>
+                    
+                </div>   
+            </div>
 
-            <div class="form-check">
-                <label class="radio-inline mr-3"><input type="radio" name="optradio" checked>Ya</label>
-                <label class="radio-inline"><input type="radio" name="optradio">Tidak</label>
-            </div>
+            
             <button type="submit" class="btn btn-danger btn-lg btn-block"><h6>Cek Status</h6></button>
         </form>
     </div>
