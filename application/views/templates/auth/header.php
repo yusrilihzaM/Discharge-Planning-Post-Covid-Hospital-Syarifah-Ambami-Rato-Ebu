26c843d87005c2d9f192cb2c27234c83762150e6
Moh Yusril Ihza Maulana <mohyusrilihzamaulana@gmail.com>
2021-11-03 13:51:43 +0700

    ui dashboard pasien

--- application/views/templates/auth/header.php
+++ application/views/templates/auth/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>Login &mdash; Stisla</title>
-
+    
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -13,7 +13,8 @@
         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- CSS Libraries -->
-    <link rel="stylesheet" href="<?= base_url() ?>../node_modules/bootstrap-social/bootstrap-social.css">
+    <link rel="stylesheet" href="<?= base_url() ?>node_modules/bootstrap-social/bootstrap-social.css">
+    <link rel="stylesheet" href="<?= base_url() ?>node_modules/prismjs/themes/prism.css">
 
     <!-- Template CSS -->
     <link rel="stylesheet" href="<?= base_url() ?>assets_user/css/style.css">
