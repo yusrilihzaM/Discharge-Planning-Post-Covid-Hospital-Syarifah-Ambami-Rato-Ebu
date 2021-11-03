71112ab5b5251ad8a62e007769b81c455b4462e4
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2021-11-03 23:41:42 +0700

    UI Deteksimandiri revisi

--- application/views/anonim/deteksi_mandiri/index.php
+++ application/views/anonim/deteksi_mandiri/index.php
@@ -3,7 +3,7 @@
             <h3 class="mb-3 mt-5">Deteksi Mandiri COVID-19</h3>
         </div>
         <div class="d-flex justify-content-center">
-            <img src="assets/img/rslogo.png" alt="" class="align-items-sm-center">
+            <img src="assets/img/rslogo.png" alt="" class="align-items-sm-center" width="100" height="125">
         </div>
         <div>
             <h6 class="d-flex justify-content-center mb-2 mt-2 ">Deteksi Mandiri Cepat COVID-19 adalah salah satu cara
@@ -16,11 +16,14 @@
 
         </div>
         <form>
-            <div class="row">
-                <div class="col">
-                    <div class="card bg-success text-white">
-                        <div class="card-body">
-                            Gejala yang dirasakan
+            <div class="card bg-success text-white">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                                Gejala yang dirasakan
+                        </div>
+                        <div class="col-md-2">
+                            Jawab
                         </div>
                     </div>
                 </div>
@@ -31,6 +34,41 @@
                     <div class="row">
                         <div class="col">
                             Gejala Hipertensi :
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            Pusing berputar
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-check">
+                                <label class="radio-inline mr-3"><input type="radio" name="optradio" checked>Ya</label>
+                                <label class="radio-inline"><input type="radio" name="optradio">Tidak</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            Tanda dan Gejala DM :
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            Sering kencing terutama malam hari
                         </div>
                         <div class="col-md-2">
                             <div class="form-check">
