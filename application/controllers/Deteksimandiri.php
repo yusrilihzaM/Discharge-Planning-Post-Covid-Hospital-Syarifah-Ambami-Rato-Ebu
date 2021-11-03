4a6fec21c4c6f1633e6bf80c0ca900c74c63ffbc
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2021-11-04 00:12:56 +0700

    Hasil Deteksimandiri

--- application/controllers/Deteksimandiri.php
+++ application/controllers/Deteksimandiri.php
@@ -13,10 +13,16 @@ class Deteksimandiri extends CI_Controller {
     public function index()
     {
         $data['title'] = 'Deteksi Mandiri';
-        $this->load->view('templates/user/header', $data); 
-		// $this->load->view('templates/user/navbar', $data); 	
-        // $this->load->view('templates/user/left_menu', $data); 	
-        $this->load->view('anonim/deteksi_mandiri/index', $data); 
+        $this->load->view('templates/user/header', $data); 	
+        $this->load->view('anonim/deteksi_mandiri/index', $data);
+        $this->load->view('templates/user/footer', $data); 	
+    }
+
+    public function hasil()
+    {
+        $data['title'] = 'Hasil';
+        $this->load->view('templates/user/header', $data); 	
+        $this->load->view('anonim/deteksi_mandiri/hasil', $data);
         $this->load->view('templates/user/footer', $data); 	
     }
 }
---
+++ application/views/anonim/deteksi_mandiri/hasil.php
@@ -0,0 +1,24 @@
+    <div class="container">
+        <div class="d-flex justify-content-center">
+            <h3 class="mb-3 mt-5">Deteksi Mandiri COVID-19</h3>
+        </div>
+        <div class="d-flex justify-content-center">
+            <img src="assets/img/rslogo.png" alt="" class="align-items-sm-center" width="100" height="125">
+        </div>
+        <div>
+            <h6 class="d-flex justify-content-center mb-2 mt-2 ">Deteksi Mandiri Cepat COVID-19 adalah salah satu cara
+                untuk membantu mempercepat tindakan</h6>
+            <h6 class="d-flex justify-content-center mb-4">apakah Anda sehat atau memiliki gejala yang memerlukan
+                pemeriksaan dan pengujian lebih lanjut mengenai COVID-19</h6>
+        </div>
+        <div class="col-12 mb-4">
+            <div class="hero align-items-center bg-success text-white">
+                <div class="hero-inner text-center">
+                    <h2>Selamat anda telah berhasil!</h2>
+                    <p class="lead">Selanjutnya, silahkan klik tombol di bawah untuk melakukan login</p>
+                    <div class="mt-4">
+                        <a href="#" class="btn btn-outline-white btn-lg btn-icon icon-left"><i class="fas fa-sign-in-alt"></i> Login</a>
+                    </div>
+                </div>
+            </div>
+        </div>
