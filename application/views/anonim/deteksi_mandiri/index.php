f52a3817a4a74ce580e1b0f1b0d4ab998516272a
Moh Yusril Ihza Maulana <mohyusrilihzamaulana@gmail.com>
2021-11-03 23:27:08 +0700

    ui deteksi mandiri belum selesai

--- application/views/anonim/deteksi_mandiri/index.php
+++ application/views/anonim/deteksi_mandiri/index.php
@@ -1,54 +1,49 @@
-    <div class="container">	
+    <div class="container">
         <div class="d-flex justify-content-center">
             <h3 class="mb-3 mt-5">Deteksi Mandiri COVID-19</h3>
         </div>
         <div class="d-flex justify-content-center">
             <img src="assets/img/rslogo.png" alt="" class="align-items-sm-center">
         </div>
-    <div>
-        <h6 class="d-flex justify-content-center mb-2 mt-2 ">Deteksi Mandiri Cepat COVID-19 adalah salah satu cara untuk membantu mempercepat tindakan</h6> 
-        <h6 class="d-flex justify-content-center mb-4">apakah Anda sehat atau memiliki gejala yang memerlukan pemeriksaan dan pengujian lebih lanjut mengenai COVID-19</h6>
-    </div>
-    <h5 class="d-flex justify-content-center mb-4 mt-2 ">Jawab Pertanyaan Berikut Untuk Memulai Deteksi Mandiri</h5> 
-    <div>
+        <div>
+            <h6 class="d-flex justify-content-center mb-2 mt-2 ">Deteksi Mandiri Cepat COVID-19 adalah salah satu cara
+                untuk membantu mempercepat tindakan</h6>
+            <h6 class="d-flex justify-content-center mb-4">apakah Anda sehat atau memiliki gejala yang memerlukan
+                pemeriksaan dan pengujian lebih lanjut mengenai COVID-19</h6>
+        </div>
+        <h5 class="d-flex justify-content-center mb-4 mt-2 ">Jawab Pertanyaan Berikut Untuk Memulai Deteksi Mandiri</h5>
+        <div>
 
-    </div>
+        </div>
         <form>
             <div class="row">
                 <div class="col">
-                        <div class="card bg-success text-white">
-                            <div class="card-body">
-                                Gejala yang dirasakan
-                            </div> 
+                    <div class="card bg-success text-white">
+                        <div class="card-body">
+                            Gejala yang dirasakan
                         </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                Gejala Hipertensi :
-                            </div> 
-                        </div>
-                </div>
-            </div>
-            <div class="row-lg">
-                <div class="card">
-                    <div class="col card-body">
-                        Pusing berputar
                     </div>
-                    
-                    <div class="form-check">
-                        <label class="radio-inline mr-3"><input type="radio" name="optradio" checked>Ya</label>
-                        <label class="radio-inline"><input type="radio" name="optradio">Tidak</label>
-                    </div>
-                    
-                </div>   
+                </div>
             </div>
 
-            
-            <button type="submit" class="btn btn-danger btn-lg btn-block"><h6>Cek Status</h6></button>
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col">
+                            Gejala Hipertensi :
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-check">
+                                <label class="radio-inline mr-3"><input type="radio" name="optradio" checked>Ya</label>
+                                <label class="radio-inline"><input type="radio" name="optradio">Tidak</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-danger btn-lg btn-block">
+                <h6>Cek Status</h6>
+            </button>
         </form>
     </div>
-
-    
