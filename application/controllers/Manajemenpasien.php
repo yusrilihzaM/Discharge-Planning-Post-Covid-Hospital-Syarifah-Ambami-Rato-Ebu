fa4e53e5f73c1a66eded4512f840aaae76416bb0
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2021-11-05 11:50:46 +0700

    Manajemen pasien

---
+++ application/controllers/Manajemenpasien.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Manajemenpasien extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->library('form_validation');
+        // $this->load->model('Manajemenpasien');
+        is_logged_in();
+    }
+
+    public function index()
+    {
+
+        $season_user=$this->session->userdata('id_user');
+        $season_patient=$this->session->userdata('id_patient');
+        if($season_user){
+            $data['user'] = $this->db->get_where('m_user', ['id_user' => $season_user])->row_array();
+            $data['name'] = $this->db->get_where('m_user', ['id_user' => $season_user])->row_array()["name"];
+        }
+        elseif($season_patient){
+            $data['user'] = $this->db->get_where('m_patient', ['id_patient' => $season_patient])->row_array();
+            $data['name'] = $this->db->get_where('m_patient', ['id_patient' => $season_patient])->row_array()["name_patient"];
+        }
+        // $data['data']=$this->Role_model->get_role();
+        $data['title'] = 'Manajemen Pasien';
+        $this->load->view('templates/user/header', $data); 
+		$this->load->view('templates/user/navbar', $data); 	
+        $this->load->view('templates/user/left_menu', $data); 	
+        $this->load->view('manajemenpasien/index', $data); 
+        $this->load->view('templates/user/footer', $data);
+    }
+}
---
+++ application/views/manajemenpasien/index.php
@@ -0,0 +1,75 @@
+<div class="main-content">
+    <section class="section">
+        <div class="flash-data-news" data-flashdata="">
+
+        </div>
+        <div class="flash-data-data" data-flashdata="">
+        </div>
+        <div class="section-header">
+            <h1><?=$title;?></h1>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+
+                        <div class="flash-data-news" data-flashdata="">
+
+                        </div>
+                        <div class="flash-data-data" data-flashdata="">
+                        </div>
+                        <a href="" type="button p-1 mb-2 "
+                            class="btn btn-success far fa-plus mx-auto">Tambah Data Pasien</a>
+                        <br>
+                        <br>
+
+                        <table id="datatable" class="table table-bordered dt-responsive nowrap"
+                            style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                            <thead>
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Nama</th>
+                                    <th scope="col">NIK</th>
+                                    <th scope="col">Tanggal lahir</th>
+                                    <th scope="col">Umur</th>
+                                    <th scope="col">Jenis Kelamin</th>
+                                    <th scope="col">Provinsi</th>
+                                    <th scope="col">Kota</th>
+                                    <th scope="col">Kecamatan</th>
+                                    <th scope="col">Kelurahan</th>
+                                    <th scope="col">Alamat</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                            
+                                <tr>
+                                    <td style="width: 4%;"></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td style="width: 15%;">
+                                        <a class="btn btn-warning  fas fa-edit" href="" style=" color:white;"></a>
+                                        <a class="btn btn-danger fas fa-trash-alt hapus-news" href=""
+                                            style="color:white;"></a>
+                                    </td>
+
+                                </tr>
+                                
+
+                            </tbody>
+
+
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+            <!-- end col -->
+        </div>
+
+    </section>
+</div>
