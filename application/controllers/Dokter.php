3607f16f78e50171e977466a82c7ec9175a8621c
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2021-11-05 20:24:01 +0700

    UI Resep Dokter

--- application/controllers/Dokter.php
+++ application/controllers/Dokter.php
@@ -29,4 +29,24 @@ class Dokter extends CI_Controller {
         $this->load->view('doktor/index', $data); 
         $this->load->view('templates/user/footer', $data); 	
     }
+
+    public function resep()
+    {
+        $data['title'] = 'Beranda Resep Dokter';
+        $season_user=$this->session->userdata('id_user');
+        $season_patient=$this->session->userdata('id_patient');
+        if($season_user){
+            $data['user'] = $this->db->get_where('m_user', ['id_user' => $season_user])->row_array();
+            $data['name'] = $this->db->get_where('m_user', ['id_user' => $season_user])->row_array()["name"];
+        }
+        elseif($season_patient){
+            $data['user'] = $this->db->get_where('m_patient', ['id_patient' => $season_patient])->row_array();
+            $data['name'] = $this->db->get_where('m_patient', ['id_patient' => $season_patient])->row_array()["name_patient"];
+        }
+        $this->load->view('templates/user/header', $data); 
+		$this->load->view('templates/user/navbar', $data); 	
+        $this->load->view('templates/user/left_menu', $data); 	
+        $this->load->view('doktor/resep', $data); 
+        $this->load->view('templates/user/footer', $data); 	
+    }
 }
---
+++ application/views/doktor/resep.php
@@ -0,0 +1,73 @@
+    <div class="main-content">
+        <section class="section">
+                <div class="section-header">
+                    <h1>Resep Dokter</h1>
+                </div>
+
+        <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                                
+                        <div class="card-header">
+                            <h4>Resep Obat Dokter</h4>
+                        </div>
+                    
+                    <div class="card-body">
+                        <form method="POST" action="<?=base_url();?>" class="needs-validation" novalidate="">
+                            <div class="form-group">
+                                <label>08 Novemver 2021</label>
+                                <br>
+                                <label>Dokter Jihan</label>
+                            </div>
+                            <div class="form-group row mb-4">
+                                <div class="col-sm-12 col-md-12">
+                                    <select class="custom-select" name="" placeholder="Kode pasien-Nama pasien"required autofocus>
+                                        <option selected disabled>Kode pasien-Nama pasien</option>
+                                        <option value="">123-Alif</option>
+                                        <option value="">234-Baba</option>
+                                        <option value="">345-Cici</option>
+                                    </select>
+                                    <div class="invalid-feedback">
+                                        Halaman tidak boleh kosong
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-4">
+                                <div class="col-sm-12 col-md-12">
+                                    <select class="custom-select" name="" placeholder="Kode resep" required autofocus>
+                                        <option selected disabled>Kode resep</option>
+                                        <option value="">prctml</option>
+                                        <option value="">c2h20</option>
+                                        <option value="">c5h2i</option>
+                                    </select>
+                                    <div class="invalid-feedback">
+                                        Kode resep tidak boleh kosong
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-4">
+                                <div class="col-sm-12 col-md-12">
+                                    <div class="form-group"  >
+                                        <label>Tuliskan resep</label>
+                                        <textarea class="form-control"  required autofocus name="" style="height: 150px;" cols="10000" placeholder="Paracetamol 3x4"></textarea>
+                                    </div>
+                                    <div class="invalid-feedback">
+                                        Halaman tidak boleh kosong
+                                    </div>
+                                </div>
+                            </div>
+
+                                    </div>
+                                        <div class="modal-footer" style="justify-content: center;">
+                                            <button type="button" class="btn btn-success mr-5" data-bs-dismiss="modal">Simpan</button>
+                                            <button type="button" class="btn btn-danger ml-5" data-bs-dismiss="modal">Keluar</button>
+                                        </div>
+
+                                </div>
+                            </div>
+                    </div>
+        </div>
+    </div>
