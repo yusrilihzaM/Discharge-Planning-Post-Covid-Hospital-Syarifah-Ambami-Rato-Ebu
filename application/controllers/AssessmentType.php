36fdd87ad39ab60aa9c3bc05d0bc85a0a9362149
Moh Yusril Ihza Maulana <mohyusrilihzamaulana@gmail.com>
2021-11-05 19:30:13 +0700

    TIpe Pertanyaan done

--- application/controllers/AssessmentType.php
+++ application/controllers/AssessmentType.php
@@ -8,24 +8,116 @@ class AssessmentType extends CI_Controller {
         parent::__construct();
 
         $this->load->library('form_validation');
+        $this->load->model('AssessmentType_model');
     
     }
     public function index()
 	{
+        $season_user=$this->session->userdata('id_user');
+        $season_patient=$this->session->userdata('id_patient');
+        if($season_user){
+            $data['user'] = $this->db->get_where('m_user', ['id_user' => $season_user])->row_array();
+            $data['name'] = $this->db->get_where('m_user', ['id_user' => $season_user])->row_array()["name"];
+        }
+        elseif($season_patient){
+            $data['user'] = $this->db->get_where('m_patient', ['id_patient' => $season_patient])->row_array();
+            $data['name'] = $this->db->get_where('m_patient', ['id_patient' => $season_patient])->row_array()["name_patient"];
+        }
+        $data['data'] = $this->db->get('assessment_type')->result_array();
 
+        $this->form_validation->set_rules('menu', 'Menu', 'required');
+        $data['title'] = 'Tipe Pertanyaan';
+
+       
+        $this->load->view('templates/user/header', $data); 
+		$this->load->view('templates/user/navbar', $data); 	
+        $this->load->view('templates/user/left_menu', $data); 	
+        $this->load->view('assessmentype/index', $data); 
+        $this->load->view('templates/user/footer', $data); 	
+    
     }
     public function add()
-	{
+    {
+
+        $season_user=$this->session->userdata('id_user');
+        $season_patient=$this->session->userdata('id_patient');
+        if($season_user){
+            $data['user'] = $this->db->get_where('m_user', ['id_user' => $season_user])->row_array();
+            $data['name'] = $this->db->get_where('m_user', ['id_user' => $season_user])->row_array()["name"];
+        }
+        elseif($season_patient){
+            $data['user'] = $this->db->get_where('m_patient', ['id_patient' => $season_patient])->row_array();
+            $data['name'] = $this->db->get_where('m_patient', ['id_patient' => $season_patient])->row_array()["name_patient"];
+        }
+        $data['menu'] = $this->db->get('assessment_type')->result_array();
+
+        $this->form_validation->set_rules('assessment_type', 'assessment_type', 'required');
+        $data['title'] = 'Tambah Tipe Pertanyaan';
+
+        if($this->form_validation->run() == false){
+        $this->load->view('templates/user/header', $data); 
+		$this->load->view('templates/user/navbar', $data); 	
+        $this->load->view('templates/user/left_menu', $data); 	
+        $this->load->view('assessmentype/add_menu', $data); 
+        $this->load->view('templates/user/footer', $data); 	
+    } else {
+        $assessment_type=$this->input->post('assessment_type');
+        $this->db->insert('assessment_type', ['assessment_type' =>$assessment_type]);
+        $this->session->set_flashdata('flash', 'Ditambahkan');
+		$this->session->set_flashdata('data', 'Tipe Pertanyaan');
+    
+        redirect('AssessmentType');
+    }
 
     }
 
-    public function put()
-	{
+    public function edit($id)
+    {
+
+        $season_user=$this->session->userdata('id_user');
+        $season_patient=$this->session->userdata('id_patient');
+        if($season_user){
+            $data['user'] = $this->db->get_where('m_user', ['id_user' => $season_user])->row_array();
+            $data['name'] = $this->db->get_where('m_user', ['id_user' => $season_user])->row_array()["name"];
+        }
+        elseif($season_patient){
+            $data['user'] = $this->db->get_where('m_patient', ['id_patient' => $season_patient])->row_array();
+            $data['name'] = $this->db->get_where('m_patient', ['id_patient' => $season_patient])->row_array()["name_patient"];
+        }
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+
+        $this->form_validation->set_rules('assessment_type', 'assessment_type', 'required');
+        $data['title'] = 'Edit Menu';
+        $data['data']=$this->db->get_where('assessment_type', ['id_assessment_type' => $id])->row_array();
+      
+        if($this->form_validation->run() == false){
+        $this->load->view('templates/user/header', $data); 
+		$this->load->view('templates/user/navbar', $data); 	
+        $this->load->view('templates/user/left_menu', $data); 	
+        $this->load->view('assessmentype/update_menu', $data); 
+        $this->load->view('templates/user/footer', $data); 	
+    } else {
+        $id_assessment_type=$this->input->post('id_assessment_type',true);
+        $assessment_type=$this->input->post('assessment_type',true);
+    
+        $this->db->set('assessment_type',$assessment_type);    
+        $this->db->where('id_assessment_type',$id_assessment_type);
+        $this->db->update('assessment_type');
+        $this->session->set_flashdata('flash', 'Di Perbarui');
+        $this->session->set_flashdata('data', 'Tipe Pertanyaan');
+        redirect('AssessmentType');
+    }
 
     }
 
-    public function del($id)
-	{
 
-    }
+    public function hapus($id_assessment_type)
+	{
+        $this->db->where('id_assessment_type', $id_assessment_type);
+        $this->db->delete('assessment_type');
+        $this->session->set_flashdata('flash', 'dihapus');
+        $this->session->set_flashdata('data', 'Tipe Pertanyaan');
+        $url="AssessmentType";
+		redirect($url);
+	}
 }
---
+++ application/views/assessmentype/add_menu.php
@@ -0,0 +1,53 @@
+<div class="main-content">
+    <section class="section">
+        <div class="flash-data-news" data-flashdata="<?= $this->session->flashdata('flash') ?>">
+
+        </div>
+        <div class="flash-data-data" data-flashdata="<?= $this->session->flashdata('data') ?>">
+        </div>
+        <div class="section-header">
+            <h1><?=$title;?></h1>
+        </div>
+
+
+        <div class="row">
+            <div class="col-12 ">
+                <div class="card">
+                    <div class="flash-data-news" data-flashdata="<?= $this->session->flashdata('flash') ?>">
+
+                    </div>
+                    <div class="flash-data-data" data-flashdata="<?= $this->session->flashdata('data') ?>">
+                    </div>
+                    <div class="card-header">
+                        <h4>Tambah Tipe pertanyaan Baru</h4>
+                    </div>
+                    <div class="card-body">
+                    <form method="POST" action="<?=base_url()?>AssessmentType/add" class="needs-validation" novalidate="">
+
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Tipe pertanyaan</label>
+                                <div class="col-sm-12 col-md-7">
+                                    <input type="text" name="assessment_type" class="form-control" placeholder="Masukan Tipe pertanyaan"required autofocus tabindex="1">
+                                    <div class="invalid-feedback">
+                                    Silahkan isi Tipe pertanyaan terlebih dahulu
+                                </div>
+                                </div>
+                                
+                            </div>
+
+                            <div class="form-group row mb-4">
+                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                <div class="col-sm-12 col-md-7">
+                                    <button type="submit" class="btn btn-success">Simpan</button>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+</div>
