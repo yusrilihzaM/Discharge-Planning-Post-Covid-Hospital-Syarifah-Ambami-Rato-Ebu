d816a2f826b9c1a196e087feb06fe0e45e3ab12d
Moh Yusril Ihza Maulana <mohyusrilihzamaulana@gmail.com>
2021-11-05 23:54:26 +0700

    deteksi mandiri loop soal

--- application/controllers/Deteksimandiri.php
+++ application/controllers/Deteksimandiri.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+date_default_timezone_set('Asia/Jakarta');
 class Deteksimandiri extends CI_Controller {
 
     public function __construct()
@@ -16,12 +16,23 @@ class Deteksimandiri extends CI_Controller {
     {
         $data['title'] = 'Deteksi Mandiri';
         $data['soal']=$this->AssessmentQuestion_model->get_assessment_all();
-
+       
         $this->load->view('templates/user/header', $data); 	
         $this->load->view('anonim/deteksi_mandiri/index', $data);
         $this->load->view('templates/user/footer', $data); 	
     }
 
+    public function hitung(){
+        $date_assessment_patient=date('Y-m-d H:i:s');
+        $id_patient=$this->session->userdata('id_patient');
+        $id_assessment_patient=$id_patient.date('ymdHi');
+        // echo ' Indonesian Timezone: ' . date('d-m-Y ').$id_assessment_patient;
+        $type=$this->db->query("SELECT * FROM assessment_type  ORDER BY id_assessment_type ASC")->result_array();
+        foreach ($type as $m){
+            $menuId = $m['id_assessment_type'];
+            $pertanyaan=$this->db->query( "SELECT * FROM assessment_question WHERE id_assessment_type = $menuId")->result_array();
+        }
+    }
     public function hasil()
     {
         $data['title'] = 'Hasil';
--- application/views/anonim/deteksi_mandiri/index.php
+++ application/views/anonim/deteksi_mandiri/index.php
@@ -3,7 +3,8 @@
             <h3 class="mb-3 mt-5">Deteksi Mandiri <span style="color :red">COVID-19</span></h3>
         </div>
         <div class="d-flex justify-content-center">
-            <img src="<?= base_url('assets/img/rslogo.png'); ?>" alt="" class="align-items-sm-center" width="100" height="125">
+            <img src="<?= base_url('assets/img/rslogo.png'); ?>" alt="" class="align-items-sm-center" width="100"
+                height="125">
         </div>
         <div>
             <h6 class="d-flex justify-content-center mb-2 mt-2 ">Deteksi Mandiri Cepat COVID-19 adalah salah satu cara
@@ -15,12 +16,13 @@
         <div>
 
         </div>
-        <form>
+        <form method="post" action="Deteksimandiri/hitung">
+
             <div class="card bg-success text-white">
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                                Gejala yang dirasakan
+                            Gejala yang dirasakan
                         </div>
                         <div class="col-md-2">
                             Jawab
@@ -28,27 +30,42 @@
                     </div>
                 </div>
             </div>
+            <?php
+          
 
-            <label for="gejala hipertensi" ><h4>Tanda dan Gejala :<span class="label label-default"></span></h4></label>
-            
+            $menu=$this->db->query("SELECT * FROM assessment_type  ORDER BY id_assessment_type ASC")->result_array();
+        ?>
+            <?php foreach ($menu as $m) : ?>
+            <label for="gejala hipertensi">
+                <h4>Tanda dan Gejala :<span class="label label-default"> <?=$m['assessment_type']?></span></h4>
+            </label>
+            <?php
+            $menuId = $m['id_assessment_type'];
+            $querySubMenu = "SELECT * 
+                                FROM assessment_question 
+                                WHERE id_assessment_type = $menuId
+                            ";
+            $subMenu = $this->db->query($querySubMenu)->result_array();
+        ?>
+         <?php foreach ($subMenu as $sm) : ?>
             <div class="card">
                 <div class="card-body">
                     <div class="row">
                         <div class="col">
-                            Pusing berputar
+                            <p><?= $sm['assessment_question']; ?></p>
                         </div>
                         <div class="col-md-2">
                             <div class="form-check">
-                                <label class="radio-inline mr-3"><input type="radio" name="optradio" checked>Ya</label>
-                                <label class="radio-inline"><input type="radio" name="optradio">Tidak</label>
+                                <label class="radio-inline"><input type="radio" value="<?= $sm['weight_yes']; ?>" name="<?= $sm['id_assessment_question']; ?>" >Ya</label>
+                                <label class="radio-inline"><input type="radio" value="<?= $sm['weight_no']; ?>"name="<?= $sm['id_assessment_question']; ?>">Tidak</label>
                             </div>
+                            
                         </div>
                     </div>
                 </div>
             </div>
-
-            <a href="<?= base_url('Deteksimandiri/hasil')?>" type="submit" class="btn btn-danger btn-lg btn-block">
-                <h6>Cek Status</h6>
-            </a>
+            <?php endforeach; ?>
+            <?php endforeach; ?>
+            <button type="submit" class="btn btn-danger btn-lg btn-block">Cek Status</button>
         </form>
     </div>
