e551fee5d14ca33417f939453536298b7c16b32a
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2021-11-06 18:49:16 +0700

    Disclaimer

---
+++ application/controllers/Disclaimer.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Disclaimer extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Disclaimer';
+        $season_user=$this->session->userdata('id_user');
+        $season_patient=$this->session->userdata('id_patient');
+        if($season_user){
+            $data['user'] = $this->db->get_where('m_user', ['id_user' => $season_user])->row_array();
+            $data['name'] = $this->db->get_where('m_user', ['id_user' => $season_user])->row_array()["name"];
+        }
+        elseif($season_patient){
+            $data['user'] = $this->db->get_where('m_patient', ['id_patient' => $season_patient])->row_array();
+            $data['name'] = $this->db->get_where('m_patient', ['id_patient' => $season_patient])->row_array()["name_patient"];
+        }
+
+        $this->load->view('templates/user/header', $data); 
+		$this->load->view('templates/user/navbar', $data); 	
+        $this->load->view('templates/user/left_menu', $data); 	
+        $this->load->view('disclaimer/index', $data); 
+        $this->load->view('templates/user/footer', $data); 
+    }
+}
---
+++ application/views/disclaimer/index.php
@@ -0,0 +1,51 @@
+    <div class="main-content">
+        <section class="section">
+                <div class="section-header">
+                    <h1 style="color: red;"><?=$title;?></h1>
+                </div>
+
+                <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                
+                        <?= $this->session->flashdata('message');?>
+                        <div class="flash-data-news" data-flashdata="<?= $this->session->flashdata('flash') ?>">
+
+                        </div>
+                        <div class="flash-data-data" data-flashdata="<?= $this->session->flashdata('data') ?>">
+                        </div>
+
+                        <a href="<?= base_url('Disclaimer'); ?>" class="btn btn-primary mb-3">Tambah data baru</a>
+
+                        <table id="datatable" class="table table-bordered dt-responsive nowrap"
+                            style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Gambar</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                
+                                    <th scope="row">1</th>
+                                    <td><img src="assets/img/profil/default.jpg" class="img-thumbnail" style="width:120px;height:120px;"></td>
+                                    <td>
+                                        <a href="<?= base_url(); ?>"
+                                            class="badge badge-success">ubah</a>
+                                        <a href="<?= base_url(); ?>"
+                                            class="badge badge-danger hapus-news">hapus</a>
+                                    </td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+
+        </div>
+    </div>
