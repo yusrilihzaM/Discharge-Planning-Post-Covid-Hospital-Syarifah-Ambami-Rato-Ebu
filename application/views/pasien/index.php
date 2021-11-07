b92679c54948dff95b1272b10f91e8bd890e6aec
rzahrohkh <170441100011@student.trunojoyo.ac.id>
2021-11-07 11:35:48 +0700

    UI Beranda pasien edit

--- application/views/pasien/index.php
+++ application/views/pasien/index.php
@@ -27,7 +27,11 @@
                     </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4 mx-auto w-100>Kontrol</h4>
+                                <h4 mx-auto w-100 style="color: black;">Kontrol</h4>
+                                
+                            </div> 
+                            <div class="card-body">
+                                <h4 class="mt-2" style="font-size: x-small;">Jadwal Kontrol Anda</h4>
                             </div>
                         </div>
                     </a>
@@ -43,10 +47,11 @@
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4 mx-auto w-100>Aktivitas</h4>
+                                <h4 mx-auto w-100 style="color: black;">Aktivitas</h4>
+                                
                             </div>
                             <div class="card-body">
-                                
+                                <h4 class="mt-2" style="font-size: x-small;">Kegiatan harian anda</h4>
                             </div>
                         </div>
                     </div>
@@ -61,10 +66,11 @@
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4 mx-auto w-100>Obat</h4>
+                                <h4 mx-auto w-100 style="color: black;">Obat</h4>
+                                
                             </div>
                             <div class="card-body">
-                                
+                                <h4 class="mt-2" style="font-size: x-small;">Obat anda saat ini</h4>
                             </div>
                         </div>
                     </div>
@@ -79,15 +85,37 @@
                         </div>
                         <div class="card-wrap">
                             <div class="card-header">
-                                <h4 mx-auto w-100>Menu Diet</h4>
+                                <h4 mx-auto w-100 style="color: black;">Menu Diet</h4>
+                                
                             </div>
                             <div class="card-body">
-                                
+                                <h4 class="mt-2" style="font-size: x-small;">Makanan yang anda konsumsi</h4>
                             </div>
                         </div>
                     </div>
                 </a>
             </div>
+
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <a href="<?= base_url('obatpasien')?>">
+                    <div class="card card-statistic-1">
+                        <div class="card-icon bg-warning">
+                            <i class="fas fa-capsules"></i>
+                        </div>
+                        <div class="card-wrap">
+                            <div class="card-header">
+                                <h4 mx-auto w-100 style="color: black;">Obat</h4>
+                                
+                            </div>
+                            <div class="card-body">
+                                <h4 class="mt-2" style="font-size: x-small;">Resep dokter</h4>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+
     </section>
 </div>
 
